1306e3bfc2f8ff1dd9ebfa4020b87b93de5e4104
Sonar Systems <admin@sonarsystems.co.uk>
2014-07-22 17:12:56 +0100

    Adding A Screen Boundary

---
+++ HelloWorldScene.cpp
@@ -0,0 +1,45 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::createWithPhysics();
+    scene->getPhysicsWorld()->setDebugDrawMask( PhysicsWorld::DEBUGDRAW_ALL );
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+    layer->SetPhysicsWorld( scene->getPhysicsWorld() );
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    
+    auto edgeBody = PhysicsBody::createEdgeBox( visibleSize, PHYSICSBODY_MATERIAL_DEFAULT, 3 );
+    
+    auto edgeNode = Node::create();
+    edgeNode ->setPosition( Point( visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y ) );
+    edgeNode->setPhysicsBody( edgeBody );
+    
+    this->addChild( edgeNode );
+    
+    return true;
+}
+
